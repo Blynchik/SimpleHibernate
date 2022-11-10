33e0fc1104b5016f6792cf572a3e773cea7abdfa
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-11-10 14:31:29 +0300

    added methods to DAO and forgotten email to model and DONE

--- HibernateAndSpring/src/main/java/ru/HibernateAndSpring/dao/PersonDAO.java
+++ HibernateAndSpring/src/main/java/ru/HibernateAndSpring/dao/PersonDAO.java
@@ -23,25 +23,36 @@ public class PersonDAO {
     public List<Person> index() {
         Session session = sessionFactory.getCurrentSession();
 
-        List<Person> people = session.createQuery("select p from Person p", Person.class)
+        return session.createQuery("select p from Person p", Person.class)
                 .getResultList();
-
-        return people;
     }
 
+    @Transactional(readOnly = true)
     public Person show(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
     }
 
+    @Transactional(readOnly = false)
     public void save(Person person) {
-
+        Session session = sessionFactory.getCurrentSession();
+        session.save(person);
     }
 
+    @Transactional(readOnly = false)
     public void update(int id, Person updatedPerson) {
+        Session session = sessionFactory.getCurrentSession();
+        Person personToBeUpdated = session.get(Person.class, id);
 
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
-
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        session.delete(person);
+        //session.remove(person);
     }
 }
--- HibernateAndSpring/src/main/java/ru/HibernateAndSpring/model/Person.java
+++ HibernateAndSpring/src/main/java/ru/HibernateAndSpring/model/Person.java
@@ -1,6 +1,7 @@
 package ru.HibernateAndSpring.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -25,13 +26,19 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+
     public Person() {
 
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, String email) {
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -56,6 +63,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
